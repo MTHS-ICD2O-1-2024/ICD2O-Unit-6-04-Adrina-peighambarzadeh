cad5639738530ff448ba0ea2dc0950a68e333a6c
default <Adrina.peighambarzadeh@stu.ocsb.ca>
2025-05-22 16:52:20 +0000

    changed everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Adrina. peighambarzadeh All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Adrina. peighambarzadeh
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+window.onload = function () {
+  // this calculates volume of a sphere
+
+  const params = new URLSearchParams(document.location.search)
+
+  // input
+  const Radius = params.get("Radius")
+  console.log(Radius)
+
+  // process
+   const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  const dimensions = `<ul><li>Radius = ("radius") mm</li></ul>`
+
+  // output
+  document.getElementById("dimensions").innerHTML = dimensions
+  document.getElementById("volume").innerHTML = "Volume is: " + volume.toFixed(2) + " mm³"
+}
